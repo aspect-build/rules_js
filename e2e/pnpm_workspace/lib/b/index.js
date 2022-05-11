70b878af5c18844b31988493dd98751d8b08bf50
Mihai Stoichitescu <mstoichi@adobe.com>
2022-05-11 18:33:09 +0300

    dot_dot issue?

--- e2e/pnpm_workspace/lib/b/index.js
+++ e2e/pnpm_workspace/lib/b/index.js
@@ -1,7 +1,10 @@
 const packageJson = require('./package.json')
+const aspect_f = require("@aspect-test/f") // expected to pass as lib/b has this dep
 module.exports = {
     id: () =>
-        `${packageJson.name}@${
-            packageJson.version ? package.version : '0.0.0'
+        `${packageJson.name}@${packageJson.version ? package.version : '0.0.0'
+        }`,
+    aspect_f: () =>
+        `${aspect_f.name}@${aspect_f.version ? aspect_f.version : '0.0.0'
         }`,
 }
