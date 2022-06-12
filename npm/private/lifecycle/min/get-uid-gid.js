23b82abae21e9827c3df3d9f96c0b6d9282da711
Alex Eagle <alex@aspect.dev>
2022-06-11 17:15:54 -0700

    re-run bundle.sh

--- npm/private/lifecycle/min/get-uid-gid.js
+++ npm/private/lifecycle/min/get-uid-gid.js
@@ -1,14 +1,14 @@
 if (module !== require.main) {
-    throw new Error('This file should not be loaded with require()')
+  throw new Error("This file should not be loaded with require()")
 }
 
 if (!process.getuid || !process.getgid) {
-    throw new Error('this file should not be called without uid/gid support')
+  throw new Error("this file should not be called without uid/gid support")
 }
 
-var argv = process.argv.slice(2),
-    user = argv[0] || process.getuid(),
-    group = argv[1] || process.getgid()
+var argv = process.argv.slice(2)
+  , user = argv[0] || process.getuid()
+  , group = argv[1] || process.getgid()
 
 if (!isNaN(user)) user = +user
 if (!isNaN(group)) group = +group
@@ -16,11 +16,9 @@ if (!isNaN(group)) group = +group
 console.error([user, group])
 
 try {
-    process.setgid(group)
-    process.setuid(user)
-    console.log(
-        JSON.stringify({ uid: +process.getuid(), gid: +process.getgid() })
-    )
+  process.setgid(group)
+  process.setuid(user)
+  console.log(JSON.stringify({uid:+process.getuid(), gid:+process.getgid()}))
 } catch (ex) {
-    console.log(JSON.stringify({ error: ex.message, errno: ex.errno }))
+  console.log(JSON.stringify({error:ex.message,errno:ex.errno}))
 }
