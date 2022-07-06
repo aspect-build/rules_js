4461884fdbc602d4d6acaf2d79ae5f41243dc64a
Jason Bedard <jason+github@jbedard.ca>
2022-07-06 16:58:27 -0700

    test: add e2e test containing transitive npm_package deps

---
+++ e2e/js_library-indirect/js_lib2/index.js
@@ -0,0 +1,4 @@
+import num1 from "@myorg/js_lib";
+import num2 from './inner';
+
+export default num1 + num2;
---
+++ e2e/js_library-indirect/main.js
@@ -0,0 +1,4 @@
+// This imports a package that has a transitive dependency
+import foo from "@myorg/js_lib2";
+
+console.log(foo);
