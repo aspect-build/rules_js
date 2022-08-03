add609f16d24b1a313e3489ebc78cf734331cc0d
John Firebaugh <john.firebaugh@gmail.com>
2022-08-03 11:40:06 -0700

    Mocha tests are escaping the sandbox

--- examples/macro/test.js
+++ examples/macro/test.js
@@ -4,4 +4,8 @@ describe('mocha', () => {
     it('integrates with Bazel', () => {
         assert(true)
     })
+
+    it('is sandboxed', () => {
+        assert.match(__dirname, /examples\/macro\/test\.sh\.runfiles/)
+    })
 })
