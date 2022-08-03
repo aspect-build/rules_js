775e8688292050e78c943cdff6b66f464c91f2f6
Greg Magolan <gmagolan@gmail.com>
2022-08-03 12:31:35 -0700

    feat: add opt-in copy_files_to_bin attribute to js_binary

--- examples/macro/test.js
+++ examples/macro/test.js
@@ -4,4 +4,8 @@ describe('mocha', () => {
     it('integrates with Bazel', () => {
         assert(true)
     })
+
+    it('is in bazel-out', () => {
+        assert.match(__dirname, /bazel-out/)
+    })
 })
