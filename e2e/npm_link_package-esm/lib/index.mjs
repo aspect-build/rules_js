5810436722a1c0b38837359fdae4e70ad7d2ba06
Jason Bedard <jason+github@jbedard.ca>
2022-09-15 09:07:24 -0700

    test: align commonjs + esm npm_link_package tests (#444)
    Closes #434

--- e2e/npm_link_package-esm/lib/index.mjs
+++ e2e/npm_link_package-esm/lib/index.mjs
@@ -1,5 +1,6 @@
 import packageJson from './package.json';
-
+import * as assert from 'uvu/assert';
+assert.is(2 + 2, 4);
 export const id = () =>
         `${packageJson.name}@${
             packageJson.version ? packageJson.version : '0.0.0'
--- e2e/npm_link_package-esm/wrapper-lib/index.mjs
+++ e2e/npm_link_package-esm/wrapper-lib/index.mjs
@@ -1,3 +1,6 @@
 export const id = () => 'wrapper-lib';
+// NOTE: .mjs does not support index files unless it is alongside a package.json
+// containing an `"exports": {".": "index.mjs"}`.
+// See: https://nodejs.org/api/packages.html#exports
 export {id as subdirId} from './subdir/index.mjs';
 export {id as libId} from '@e2e/lib';
