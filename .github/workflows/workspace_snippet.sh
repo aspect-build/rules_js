42b19d19611d307e959aab957a5c80da5d53d753
Alex Eagle <alex@aspect.dev>
2022-10-21 16:39:46 -0700

    chore: document bzlmod usage in release notes (#545)

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -6,11 +6,29 @@ set -o errexit -o nounset -o pipefail
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
 PREFIX="rules_js-${TAG:1}"
-SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+SHA=$(git archive --format=tar --prefix="${PREFIX}/" "${TAG}" | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
 
-WORKSPACE snippet:
+## Using Bzlmod with Bazel 6:
+
+Add to your \`MODULE.bazel\` file:
+\`\`\`starlark
+bazel_dep(name = "aspect_rules_js", version = "${TAG:1}")
+
+npm = use_extension("@aspect_rules_js//npm:extensions.bzl", "npm", dev_dependency = True)
+
+npm.npm_translate_lock(
+    name = "npm",
+    pnpm_lock = "//:pnpm-lock.yaml",
+)
+
+use_repo(npm, "npm")
+\`\`\`
+
+## Using WORKSPACE
+
+Paste this snippet into your \`WORKSPACE\` file:
 
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
