8de2fe6556b900c99e2b9dde690f5b8cffdacb19
Greg Magolan <gmagolan@gmail.com>
2022-11-23 12:02:35 -0800

    fix: don't override JS_BINARY__FS_PATCH_ROOTS if already set (#637)

---
+++ e2e/webpack_devserver/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# Integration test for js_run_devserver run with ibazel
+
+bazel run -- @pnpm//:pnpm --dir "$PWD" install
+
+./serve_test.sh //:dev
