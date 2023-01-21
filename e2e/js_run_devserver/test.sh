86d7151bf98c4bcf92ebe8bdd25e33d90579c053
Derek Cormier <derek@aspect.dev>
2023-01-20 16:58:55 -0800

    chore: run e2e/js_run_devserver under bzlmod

--- e2e/js_run_devserver/test.sh
+++ e2e/js_run_devserver/test.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail
 
+BZLMOD_FLAG="${BZLMOD_FLAG:-}"
+
 # Integration test for js_run_devserver run with ibazel
 
-bazel run -- @pnpm//:pnpm --dir "$PWD" install
+bazel run $BZLMOD_FLAG -- @pnpm//:pnpm --dir "$PWD" install
 
 ./serve_test.sh //src:serve
 ./serve_test.sh //src:serve_alt
