82f1aa72af8bb166f0d0b7b44d89024670841f68
Derek Cormier <derek@aspect.dev>
2023-02-13 00:04:03 -0800

    enhancement: use sourcemaps on lifecycle hook bundle to clarify strack trace output

--- npm/private/lifecycle/bundle.sh
+++ npm/private/lifecycle/bundle.sh
@@ -13,8 +13,4 @@ set -o errexit -o nounset
 # This avoids users having to fetch all those packages just to run the postinstall hooks.
 
 npm install
-npx -y @vercel/ncc@0.33.4 build lifecycle-hooks.js -o min
-# ascii_only avoids bad unicode conversions, fixing
-# https://github.com/aspect-build/rules_js/issues/45
-npx -y terser@5.12.1 min/index.js -b ascii_only=true > min/index.min.js
-rm min/index.js
+npx -y @vercel/ncc@0.33.4 build lifecycle-hooks.js --out min --minify --source-map
