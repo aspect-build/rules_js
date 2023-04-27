3a86a8c1c7206a3af895ffdec9dd035bdc6f74de
Jason Bedard <jason+github@jbedard.ca>
2023-04-26 23:34:28 -0700

    fix: cleanup flaky devserver tests (#1026)
    * test: exit on error in js_run_devserver test server
    
    Ref https://github.com/aspect-build/rules_js/issues/1025
    
    * test: wait for ibazel to exit before test exit in js_run_devserver test
    
    Ref https://github.com/aspect-build/rules_js/issues/1025
    
    * test: poll for js_run_devserver changes instead of timeout, add timeout to initial wait
    
    Ref https://github.com/aspect-build/rules_js/issues/1025
    
    * test: prefix js_run_devserver log statements with pid
    
    Ref https://github.com/aspect-build/rules_js/issues/1025
    
    * test: dont surpress bazel stdout in js_run_devserver tests
    
    Ref https://github.com/aspect-build/rules_js/issues/1025

--- e2e/js_run_devserver/multirun_test.sh
+++ e2e/js_run_devserver/multirun_test.sh
@@ -14,107 +14,119 @@ _sedi () {
   sed "${sedi[@]}" "$@"
 }
 
-echo "TEST - $0: $1"
+echo "$$: TEST - $0: $1"
 
-./node_modules/.bin/ibazel run "$1" "$BZLMOD_FLAG" >/dev/null 2>&1 &
+./node_modules/.bin/ibazel run "$1" "$BZLMOD_FLAG" 2>&1 &
 ibazel_pid="$!"
 
 function _exit {
-  echo "Cleanup..."
+  echo "$$: Cleanup..."
   kill "$ibazel_pid"
+  wait "$ibazel_pid"
   git checkout src/index.html src/BUILD.bazel
   rm -f src/new.html
 }
 trap _exit EXIT
 
-echo "Waiting for $1 devserver to launch on 8080..."
+echo "$$: Waiting for $1 devserver to launch on 8080..."
 
+n=0
 while ! nc -z localhost 8080; do
-  echo "... waiting (8080)"
-  sleep 0.5 # wait before check again
+  if [ $n -gt 100 ]; then
+    echo "$$: ERROR: Expected http://localhost:8080 to be available"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
 done
 
-echo "Waiting for $1 devserver to launch on 8081..."
+echo "$$: Waiting for $1 devserver to launch on 8081..."
 
+n=0
 while ! nc -z localhost 8081; do
-  echo "... waiting (8081)"
-  sleep 0.5 # wait before check again
+  if [ $n -gt 100 ]; then
+    echo "$$: ERROR: Expected http://localhost:8081 to be available"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
 done
 
-echo "Waiting 10 seconds for devservers to settle to mitigate flakiness..."
-sleep 10
-
-echo "Devservers ready"
+echo "$$: Devservers ready"
 
 if ! curl http://localhost:8080/index.html --fail 2>/dev/null | grep "My first website"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'My first website'"
+  echo "$$: ERROR: Expected http://localhost:8080/index.html to contain 'My first website'"
   exit 1
 fi
 
 if ! curl http://localhost:8081/index.html --fail 2>/dev/null | grep "My first website"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'My first website'"
+  echo "$$: ERROR: Expected http://localhost:8081/index.html to contain 'My first website'"
   exit 1
 fi
 
 if ! curl http://localhost:8080/other.html --fail 2>/dev/null | grep "My other website"; then
-  echo "ERROR: Expected http://localhost:8080/other.html to contain 'My other website'"
+  echo "$$: ERROR: Expected http://localhost:8080/other.html to contain 'My other website'"
   exit 1
 fi
 
 if ! curl http://localhost:8081/other.html --fail 2>/dev/null | grep "My other website"; then
-  echo "ERROR: Expected http://localhost:8080/other.html to contain 'My other website'"
+  echo "$$: ERROR: Expected http://localhost:8081/other.html to contain 'My other website'"
   exit 1
 fi
 
 if curl http://localhost:8080/new.html --fail 2>/dev/null; then
-  echo "ERROR: Expected http://localhost:8080/new.html to fail with 404"
+  echo "$$: ERROR: Expected http://localhost:8080/new.html to fail with 404"
   exit 1
 fi
 
 if curl http://localhost:8081/new.html --fail 2>/dev/null; then
-  echo "ERROR: Expected http://localhost:8080/new.html to fail with 404"
+  echo "$$: ERROR: Expected http://localhost:8081/new.html to fail with 404"
   exit 1
 fi
 
 if curl http://localhost:8080/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to NOT contain 'A second line'"
+  echo "$$: ERROR: Expected http://localhost:8080/index.html to NOT contain 'A second line'"
   exit 1
 fi
 
 if curl http://localhost:8081/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to NOT contain 'A second line'"
+  echo "$$: ERROR: Expected http://localhost:8081/index.html to NOT contain 'A second line'"
   exit 1
 fi
 
-echo "<div>A second line</div>" >> src/index.html
+echo "$$: <div>A second line</div>" >> src/index.html
 
-echo "Waiting 10 seconds for ibazel rebuild after change to src/index.html..."
-sleep 10
-
-if ! curl http://localhost:8080/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'A second line'"
-  exit 1
-fi
+n=0
+while ! curl http://localhost:8080/index.html --fail 2>/dev/null | grep "A second line"; do
+  if [ $n -gt 30 ]; then
+    echo "$$: ERROR: Expected http://localhost:8080/index.html to contain 'A second line'"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
+done
 
 if ! curl http://localhost:8081/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'A second line'"
+  echo "$$: ERROR: Expected http://localhost:8081/index.html to contain 'A second line'"
   exit 1
 fi
 
-echo "<div>A new file</div>" > src/new.html
+echo "$$: <div>A new file</div>" > src/new.html
 _sedi 's#"other.html"#"other.html", "new.html"#' src/BUILD.bazel
 
-echo "Waiting 10 seconds for ibazel rebuild after change to src/BUILD.bazel..."
-sleep 10
-
-if ! curl http://localhost:8080/new.html --fail 2>/dev/null | grep "A new file"; then
-  echo "ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
-  exit 1
-fi
+n=0
+while ! curl http://localhost:8080/new.html --fail 2>/dev/null | grep "A new file"; do
+  if [ $n -gt 30 ]; then
+    echo "$$: ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
+done
 
 if ! curl http://localhost:8081/new.html --fail 2>/dev/null | grep "A new file"; then
-  echo "ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
+  echo "$$: ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
   exit 1
 fi
 
-echo "All tests passed"
+echo "$$: All tests passed"
--- e2e/js_run_devserver/serve_test.sh
+++ e2e/js_run_devserver/serve_test.sh
@@ -14,70 +14,78 @@ _sedi () {
   sed "${sedi[@]}" "$@"
 }
 
-echo "TEST - $0: $1"
+echo "$$: TEST - $0: $1"
 
-./node_modules/.bin/ibazel run "$1" "$BZLMOD_FLAG" >/dev/null 2>&1 &
+./node_modules/.bin/ibazel run "$1" "$BZLMOD_FLAG" 2>&1 &
 ibazel_pid="$!"
 
 function _exit {
-  echo "Cleanup..."
+  echo "$$: Cleanup..."
   kill "$ibazel_pid"
+  wait "$ibazel_pid"
   git checkout src/index.html src/BUILD.bazel
   rm -f src/new.html
 }
 trap _exit EXIT
 
-echo "Waiting for $1 devserver to launch on 8080..."
+echo "$$: Waiting for $1 devserver to launch on 8080..."
 
+n=0
 while ! nc -z localhost 8080; do
-  echo "."
-  sleep 0.5 # wait before check again
+  if [ $n -gt 100 ]; then
+    echo "$$: ERROR: Expected http://localhost:8080 to be available"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
 done
 
-echo "Waiting 5 seconds for devservers to settle..."
+echo "$$: Waiting 5 seconds for devservers to settle..."
 sleep 5
 
-echo "Devserver ready"
+echo "$$: Devserver ready"
 
 if ! curl http://localhost:8080/index.html --fail 2>/dev/null | grep "My first website"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'My first website'"
+  echo "$$: ERROR: Expected http://localhost:8080/index.html to contain 'My first website'"
   exit 1
 fi
 
 if ! curl http://localhost:8080/other.html --fail 2>/dev/null | grep "My other website"; then
-  echo "ERROR: Expected http://localhost:8080/other.html to contain 'My other website'"
+  echo "$$: ERROR: Expected http://localhost:8080/other.html to contain 'My other website'"
   exit 1
 fi
 
 if curl http://localhost:8080/new.html --fail 2>/dev/null; then
-  echo "ERROR: Expected http://localhost:8080/new.html to fail with 404"
+  echo "$$: ERROR: Expected http://localhost:8080/new.html to fail with 404"
   exit 1
 fi
 
 if curl http://localhost:8080/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to NOT contain 'A second line'"
+  echo "$$: ERROR: Expected http://localhost:8080/index.html to NOT contain 'A second line'"
   exit 1
 fi
 
-echo "<div>A second line</div>" >> src/index.html
+echo "$$: <div>A second line</div>" >> src/index.html
 
-echo "Waiting 5 seconds for ibazel rebuild after change to src/index.html..."
+echo "$$: Waiting 5 seconds for ibazel rebuild after change to src/index.html..."
 sleep 5
 
 if ! curl http://localhost:8080/index.html --fail 2>/dev/null | grep "A second line"; then
-  echo "ERROR: Expected http://localhost:8080/index.html to contain 'A second line'"
+  echo "$$: ERROR: Expected http://localhost:8080/index.html to contain 'A second line'"
   exit 1
 fi
 
-echo "<div>A new file</div>" > src/new.html
+echo "$$: <div>A new file</div>" > src/new.html
 _sedi 's#"other.html"#"other.html", "new.html"#' src/BUILD.bazel
 
-echo "Waiting 10 seconds for ibazel rebuild after change to src/BUILD.bazel..."
-sleep 10
+n=0
+while ! curl http://localhost:8080/new.html --fail 2>/dev/null | grep "A new file"; do
+  if [ $n -gt 60 ]; then
+    echo "$$: ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
+    exit 1
+  fi
+  sleep 1 # wait before check again
+  ((n=n+1))
+done
 
-if ! curl http://localhost:8080/new.html --fail 2>/dev/null | grep "A new file"; then
-  echo "ERROR: Expected http://localhost:8080/new.html to contain 'A new file'"
-  exit 1
-fi
-
-echo "All tests passed"
+echo "$$: All tests passed"
