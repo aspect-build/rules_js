cb46fbda66a1f0c748f31efdd18db8d47cd2301d
TarunGunampalli <34664042+TarunGunampalli@users.noreply.github.com>
2023-08-11 14:25:39 -0700

    created failing bazel run for js_image_docker e2e test (#1203)

--- e2e/js_image_docker/src/main.js
+++ e2e/js_image_docker/src/main.js
@@ -1,28 +1,22 @@
-const chalk = require('chalk')
-const fs = require('fs')
-const os = require('os')
+import { spawn } from 'node:child_process'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const space = ' '
-const art = fs.readFileSync(__dirname + '/ascii.art')
+const __esm_filename = fileURLToPath(import.meta.url)
+const __esm_dirname = path.dirname(__esm_filename)
 
-console.log(chalk.italic.green(art))
-console.log(
-    chalk.italic.bgBlue(' WORKSPACE '),
-    space,
-    chalk.blueBright(process.env.JS_BINARY__WORKSPACE)
-)
-console.log(
-    chalk.bold.bgGreen(' TARGET '),
-    space,
-    chalk.greenBright(process.env.JS_BINARY__TARGET)
-)
-console.log(
-    chalk.bold.bgGray(' ARCH/CPU '),
-    space,
-    chalk.gray(process.env.JS_BINARY__TARGET_CPU)
-)
-console.log(
-    chalk.bold.bgRed(' OS '),
-    space,
-    chalk.redBright(process.platform, os.version(), os.arch())
-)
+const child = spawn('node', ['./node_modules/chalk/source/index.js'], {
+  cwd: __esm_dirname,
+})
+
+child.stdout.on('data', data => {
+  console.log(`stdout: ${data}`)
+})
+
+child.stderr.on('data', data => {
+  console.error(`stderr: ${data}`)
+})
+
+child.on('close', code => {
+  console.log(`child process exited with code ${code}`)
+})
