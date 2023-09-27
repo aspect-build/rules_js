0b5e01dd3f453311190a24d6c130877cbcb29ab4
Greg Magolan <greg@aspect.dev>
2023-09-27 14:25:17 -0700

    chore: upgrade to Aspect Workflows 5.8.0-rc9

--- .aspect/workflows/terraform/main.tf
+++ .aspect/workflows/terraform/main.tf
@@ -7,7 +7,9 @@ terraform {
   }
 }
 
-provider "google" {
+locals {
+  # Project & region of the Workflows deployment. Alternately, you may configure a global `provider
+  # "google"` with the desired project & region and the Workflows module will default to that.
   project = "aw-deployment-rules-js"
   region  = "us-west2"
 }
