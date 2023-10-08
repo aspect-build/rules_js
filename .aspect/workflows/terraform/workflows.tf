c50bd3f797c501fb229cf9ab58e0e4fc11464a2f
Greg Magolan <gmagolan@gmail.com>
2023-10-08 14:13:50 -0700

    chore: upgrade to Aspect Workflows 5.8.0-rc14 (#1309)

--- .aspect/workflows/terraform/workflows.tf
+++ .aspect/workflows/terraform/workflows.tf
@@ -16,7 +16,7 @@ module "aspect_workflows" {
   region  = local.region
 
   # Aspect Workflows terraform module
-  source = "https://s3.us-east-2.amazonaws.com/static.aspect.build/aspect/5.8.0-rc12/workflows-gcp/terraform-gcp-aspect-workflows.zip"
+  source = "https://s3.us-east-2.amazonaws.com/static.aspect.build/aspect/5.8.0-rc14/workflows-gcp/terraform-gcp-aspect-workflows.zip"
 
   # Network properties
   network    = google_compute_network.workflows_network.id
