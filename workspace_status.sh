ed46bba17a0a6cccd6ac77dcaaeb0a712706a1a2
Greg Magolan <gmagolan@gmail.com>
2024-04-26 12:28:58 -0700

    chore: tweak delivery stamping (#1685)

--- workspace_status.sh
+++ workspace_status.sh
@@ -2,4 +2,4 @@
 # See https://bazel.build/docs/user-manual#workspace-status
 
 # Starts with the STABLE_ prefix so that Bazel will always rebuild stamped outputs if the git tag changes.
-echo STABLE_BUILD_VERSION "$(git describe --long | sed -e 's/^v//;s/-/./;s/-g/+/')"
+echo STABLE_BUILD_VERSION "$(git describe --tags --long | sed -e 's/^v//;s/-/./;s/-g/+/')"
