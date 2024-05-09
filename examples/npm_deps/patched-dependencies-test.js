b66dad13aba6449225c9e1f62637d96cca691bcc
Jason Bedard <jason+github@jbedard.ca>
2024-05-09 11:55:57 -0700

    fix: do not support mixing pnpm.patchedDependencies and npm_translate_lock(patches) (#1694)
    Fix #1427
    Close #1693
    Close #837

--- examples/npm_deps/patched-dependencies-test.js
+++ examples/npm_deps/patched-dependencies-test.js
@@ -1,19 +1,12 @@
 const meaningOfLife = require('meaning-of-life')
 
-// meaning-of-life should have been patched twice:
-//
-// First, by the `pnpm.patchedDependencies` patch:
+// Verify meaning-of-life was patched with
 //  examples/npm_deps/patches/meaning-of-life@1.0.0-pnpm.patch
-// 42 => "forty two"
-//
-// Then by the the following patch in the `patches` attr:
-//  examples/npm_deps/patches/meaning-of-life@1.0.0-after_pnpm.patch
-// "forty two" => 32
 
 if (meaningOfLife === 42) {
     throw new Error('Patches were not applied!')
-} else if (meaningOfLife === 'forty two') {
-    throw new Error('Only `pnpm.patchedDependencies` patch was applied!')
-} else if (meaningOfLife !== 32) {
-    throw new Error('Patch in `patches` was not applied!')
+}
+
+if (meaningOfLife !== 'forty two') {
+    throw new Error('`pnpm.patchedDependencies` was not applied!')
 }
