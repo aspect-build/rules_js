74147beffbbd5b072c114167497a7c56bfaba797
Jason Bedard <jason+github@jbedard.ca>
2024-06-28 18:01:39 -0700

    fix: transitive js_library npm package deps

--- e2e/pnpm_workspace_rerooted/lib/d/index.js
+++ e2e/pnpm_workspace_rerooted/lib/d/index.js
@@ -1,9 +1,12 @@
 const packageJson = require('./package.json')
-const a2 = require('alias-2/package.json')
 module.exports = {
     id: () =>
         `${packageJson.name}@${
             packageJson.version ? packageJson.version : '0.0.0'
         }`,
-    test: () => [a2.name].join('\n'),
+    test: () =>
+        [
+            require('alias-2/package.json').name,
+            require('@aspect-test/d').version,
+        ].join('\n'),
 }
