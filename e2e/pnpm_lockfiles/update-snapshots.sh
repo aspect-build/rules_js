d59eaa33d72891227059cc059c8e8cca19480387
Jason Bedard <jason+github@jbedard.ca>
2025-07-24 16:48:38 -0700

    test: stabilize pnpm_lockfiles versions (#2281)

--- e2e/pnpm_lockfiles/update-snapshots.sh
+++ e2e/pnpm_lockfiles/update-snapshots.sh
@@ -4,10 +4,10 @@ bazel run @@//v54:repos
 bazel run @@//v60:repos
 bazel run @@//v61:repos
 bazel run @@//v90:repos
-bazel run @@//v10:repos
+bazel run @@//v101:repos
 
 bazel run --enable_bzlmod=false @@//v54:wksp-repos
 bazel run --enable_bzlmod=false @@//v60:wksp-repos
 bazel run --enable_bzlmod=false @@//v61:wksp-repos
 bazel run --enable_bzlmod=false @@//v90:wksp-repos
-bazel run --enable_bzlmod=false @@//v10:wksp-repos
+bazel run --enable_bzlmod=false @@//v101:wksp-repos
