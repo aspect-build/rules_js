ba5ae34e7b8b896064fcbb6379d1600cef290498
Mihail Vratchanski <mivrkiki@gmail.com>
2025-09-29 20:31:27 +0300

    Make the e2e test pass, while testing the real changes in a negative test

--- e2e/npm_translate_lock_package_visibility/test.sh
+++ e2e/npm_translate_lock_package_visibility/test.sh
@@ -3,6 +3,13 @@ set -o errexit -o nounset -o pipefail -o xtrace
 
 BZLMOD_FLAG="${BZLMOD_FLAG:---enable_bzlmod=1}"
 
+# Change visibility to restricted (since bazel test //... already validated the public case works)
+sed -i 's/"some-dep": \["\/\/visibility:public"\]/"some-dep": ["\/\/packages\/nonexistent:__subpackages__"]/' MODULE.bazel
+sed -i 's/"some-dep": \["\/\/visibility:public"\]/"some-dep": ["\/\/packages\/nonexistent:__subpackages__"]/' WORKSPACE
+
+# Clear Bazel cache to ensure new visibility is loaded
+bazel clean --expunge 2>/dev/null || true
+
 # Test that package_visibility restrictions are enforced for local node_modules references
 build_output=$(bazel build "$BZLMOD_FLAG" //packages/from-local:from_local_lib 2>&1 || true)
 if ! echo "$build_output" | grep -q "is not visible from"; then
