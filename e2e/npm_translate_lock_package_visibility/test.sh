d1b79e80d894a139625ec384367a1ec0b09295bf
Jason Bedard <jason+github@jbedard.ca>
2026-01-12 16:33:36 -0800

    wip: mac fix

--- e2e/npm_translate_lock_package_visibility/test.sh
+++ e2e/npm_translate_lock_package_visibility/test.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o xtrace
 
+# sedi makes `sed -i` work on both OSX & Linux
+# See https://stackoverflow.com/questions/2320564/i-need-my-sed-i-command-for-in-place-editing-to-work-with-both-gnu-sed-and-bsd
+_sedi() {
+    case $(uname) in
+    Darwin*) sedi=('-i' '') ;;
+    *) sedi=('-i') ;;
+    esac
+
+    sed "${sedi[@]}" "$@"
+}
+
 # Change visibility to restricted (since bazel test //... already validated the public case works)
-sed -i 's/"some-dep": \["\/\/visibility:public"\]/"some-dep": ["\/\/packages\/nonexistent:__subpackages__"]/' MODULE.bazel
+_sedi 's/"some-dep": \["\/\/visibility:public"\]/"some-dep": ["\/\/packages\/nonexistent:__subpackages__"]/' MODULE.bazel
 
 # Clear Bazel cache to ensure new visibility is loaded
 bazel clean --expunge 2>/dev/null || true
