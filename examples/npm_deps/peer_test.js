58e5cfe377be40bfc06d0b51265f91c43a584052
Jason Bedard <jason+github@jbedard.ca>
2026-01-19 18:29:53 -0800

    chore: upgrade all rollup packages

--- examples/npm_deps/peer_test.js
+++ examples/npm_deps/peer_test.js
@@ -1,10 +1,8 @@
-const cjs = require('@rollup/plugin-commonjs/package.json')
 const assert = require('assert')
 const react = require('react')
 const mobx = require('mobx-react/package.json')
 assert.equal(react.version, '17.0.2')
 assert.equal(mobx.version, '7.3.0')
-assert.equal(cjs.version, '21.1.0')
 
 // Ensure the main package with peer dependencies works too
 require('@rollup/plugin-commonjs')
