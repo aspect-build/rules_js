135c4f521f5209bb20ddcd16ce2f94e3c95a8df1
Tyler Breisacher <matrixfrog@gmail.com>
2026-03-24 17:56:11 -0700

    Drop fallback for Node 19 and below

--- examples/nextjs/next.standalone.mjs
+++ examples/nextjs/next.standalone.mjs
@@ -1,9 +1,7 @@
 import { join } from 'node:path'
 
-const appDir = import.meta.dirname || dirname(fileURLToPath(import.meta.url))
-
 // Must include the parent .aspect_rules_js package store when tracing for standalone output
-const outputFileTracingRoot = join(appDir, '../../')
+const outputFileTracingRoot = join(import.meta.dirname, '../../')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
