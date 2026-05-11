cd852e48d17f6fa3eb25ea2c67943afb422ffe3e
Jason Bedard <jason+github@jbedard.ca>
2026-05-11 12:30:34 -0700

    Remove check for acorn module existence

--- e2e/js_image_oci/src/main.js
+++ e2e/js_image_oci/src/main.js
@@ -86,9 +86,5 @@ console.log(
 console.log(
     chalk.bold.bgCyan(' REPO NPM CHECK '),
     space,
-    chalk.cyanBright(
-        fs.existsSync(
-            path.join(process.env.JS_BINARY__RUNFILES, 'repo/node_modules/acorn')
-        )
-    )
+    chalk.cyanBright(runfileExists('repo/node_modules/acorn')))
 )
